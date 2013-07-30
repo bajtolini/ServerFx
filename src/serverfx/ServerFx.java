177d3d0cd2c429be76c0ceba046d33be7c0cb17c
bajtolini <bajtolini@gmail.com>
2013-07-30 13:11:00 +0200

    Server created from Server rep, working for ClientFx, added one feature: when client connect to server he got message about the number of clients currently connected to server, erased one feature: the command ".bye" which previously enabled client to disconnect from server now is erased (whith gui its useless). When client is disconnecting from server the server cannot tell the difference between error disconnect and normal one that's why when client disconnect server write error.

---
+++ src/serverfx/ServerFx.java
@@ -0,0 +1,112 @@
+package serverfx;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerFx implements Runnable {
+
+    private ServerThread clients[] = new ServerThread[50];
+    private ServerSocket server = null;
+    private Thread thread = null;
+    private int clientCount = 0;
+
+    public ServerFx(int port) {
+        try {
+            InetAddress adress = InetAddress.getLocalHost();
+            System.out.println("Binding to port " + port + ", please wait  ...");
+            server = new ServerSocket(port, 50, adress);
+            System.out.println("Server started: " + server);
+            start();
+        } catch (IOException ioe) {
+            System.err.println("Can not bind to port " + port + ": " + ioe.getMessage());
+        }
+    }
+
+    @Override
+    public void run() {
+        while (thread != null) {
+            try {
+                System.out.println("Waiting for a client ...");
+                addThread(server.accept());
+            } catch (IOException ioe) {
+                System.out.println("Server accept error: " + ioe);
+                stop();
+            }
+        }
+    }
+
+    public void start() {
+        if (thread == null) {
+            thread = new Thread(this);
+            thread.start();
+        }
+    }
+
+    public void stop() {
+        if (thread != null) {
+            thread = null;
+        }
+    }
+
+    private int findClient(int ID) {
+        for (int i = 0; i < clientCount; i++) {
+            if (clients[i].getID() == ID) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public synchronized void handle(int ID, String input) {
+        for (int i = 0; i < clientCount; i++) {
+            if (clients[findClient(ID)] != clients[i]) {
+                clients[i].send(input);
+            }
+        }
+    }
+
+    public synchronized void remove(int ID) {
+        int pos = findClient(ID);
+        if (pos >= 0) {
+            ServerThread toTerminate = clients[pos];
+            System.out.println("Removing client thread " + ID + " at " + pos);
+            if (pos < clientCount - 1) {
+                for (int i = pos + 1; i < clientCount; i++) {
+                    clients[i - 1] = clients[i];
+                }
+            }
+            clientCount--;
+            try {
+                toTerminate.close();
+            } catch (IOException ioe) {
+                System.err.println("Error closing thread: " + ioe);
+            }
+        }
+    }
+
+    private void addThread(Socket socket) {
+        if (clientCount < clients.length) {
+            System.out.println("Client accepted: " + socket);
+            clients[clientCount] = new ServerThread(this, socket);
+            try {
+                clients[clientCount].open(clientCount);
+                clients[clientCount].start();
+                clientCount++;
+            } catch (IOException ioe) {
+                System.err.println("Error opening thread: " + ioe);
+            }
+        } else {
+            System.out.println("Client refused: maximum " + clients.length + " reached.");
+        }
+    }
+
+    public static void main(String args[]) {
+        if (args.length != 1) {
+            System.err.println("Usage: wrong port, type once more");
+        } else {
+            ServerFx server = new ServerFx(Integer.parseInt(args[0]));
+        }
+    }
+}
---
+++ src/serverfx/ServerThread.java
@@ -0,0 +1,83 @@
+package serverfx;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+
+public class ServerThread extends Thread {
+
+    private boolean stop;
+    private ServerFx server = null;
+    private Socket socket = null;
+    private int ID = 0;
+    private DataInputStream streamIn = null;
+    private DataOutputStream streamOut = null;
+    private int clientCount;
+
+
+    public ServerThread(ServerFx _server, Socket _socket) {
+        super();
+        this.stop = false;
+        server = _server;
+        socket = _socket;
+        ID = socket.getPort();
+    }
+
+    public void send(String msg) {
+        try {
+            streamOut.writeUTF(msg);
+            streamOut.flush();
+        } catch (IOException ioe) {
+            System.err.println(ID + " ERROR sending: " + ioe.getMessage());
+            server.remove(ID);
+        }
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public void run() {
+        clientCount++;
+        System.out.println("Server Thread " + ID + " running.");
+        if (clientCount == 1) {
+            send("Welcome to Chat, currently: " + clientCount + " user online");
+        } else {
+            send("Welcome to Chat, currently: " + clientCount + " users online");
+        }
+        while (!this.stop) {
+            try {
+                server.handle(ID, streamIn.readUTF());
+            } catch (IOException ioe) {
+                System.err.println(ID + " ERROR reading: " + ioe.getMessage());
+                server.remove(ID);
+                break;
+            }
+        }
+    }
+
+    public void open(int clients) throws IOException {
+        streamIn = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
+        streamOut = new DataOutputStream(new BufferedOutputStream(socket.getOutputStream()));
+        clientCount = clients;
+    }
+
+    public void close() throws IOException {
+        if (socket != null) {
+            socket.close();
+        }
+        if (streamIn != null) {
+            streamIn.close();
+        }
+        if (streamOut != null) {
+            streamOut.close();
+        }
+    }
+    
+    public void finish() {
+        this.stop = true;
+    }
+}
